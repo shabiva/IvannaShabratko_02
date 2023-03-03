ff973b0012bf4f62a9099f8d642b6941a0ddf274
shabiva <zubenyu@gmail.com>
2023-03-03 15:00:32 +0000

    first commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+
+
+let a = prompt("Type first number");
+let b = prompt("Type second number");
+let sum = Number(a) + Number(b);
+let dif = a - b;
+let mul = a * b;
+let divis = a / b;
+alert('Sum ' + '= ' + sum + ' Dif ' + '= ' + dif + ' Mul ' + '= ' + mul + ' Divis ' + '= ' + divis);
+
